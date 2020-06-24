9715a857688a0a03e2d88c63751834e657fc0aa1
陈芊芊 <813928419@qq.com>
2020-06-24 09:04:50 +0800

    删除old

--- product/src/components/commen/index.js
+++
@@ -1,6 +0,0 @@
-import Vue from "vue"
-const requireContext = require.context("./",true,/\.vue$/)
-requireContext.keys().forEach(filename=>{
-    const route = requireContext(filename)
-    Vue.component(route.default.name,route.default)
-})
